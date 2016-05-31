8777dd5f8ef723000bd007d712362fde0060a6af
maxbarry <maxbarrymail@gmail.com>
2016-05-31 12:38:43 +0100

    NODE_ENV set as production on prod builds

--- generators/app/templates/gulp/webpack.config.js
+++ generators/app/templates/gulp/webpack.config.js
@@ -36,7 +36,12 @@ var webpackSettings = {
     },
     plugins: environments.production() ? [
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin()
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
     ] : [
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
