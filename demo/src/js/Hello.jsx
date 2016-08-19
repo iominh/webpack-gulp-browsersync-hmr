fdd5ea185ebe0ac6a8277b99b915863d6f2489d5
Minh Nguyen <minh.9.nguyen@gmail.com>
2016-08-18 23:37:00 -0400

    demo using generator-jumpstart-react with webpack dashboard and HMR for JS+SCSS

---
+++ demo/src/js/Hello.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import './styles.scss';
+
+class Hello extends React.Component {
+
+	constructor(props){
+		super(props);
+	}
+
+	render() {
+		return(
+			<div>
+				Hello world🐱🐱
+			</div>
+
+		);
+	}
+
+}
+
+export default Hello;
---
+++ demo/src/js/app.jsx
@@ -0,0 +1,11 @@
+/*eslint-disable no-unused-vars*/
+import React from 'react';
+/*eslint-enable no-unused-vars*/
+import { render } from 'react-dom';
+
+import ReactDOM from 'react';
+import Hello from './Hello.jsx';
+
+render(
+	<Hello />
+, document.getElementById('react-root'));
